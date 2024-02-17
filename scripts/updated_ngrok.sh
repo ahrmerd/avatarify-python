e44a31f18e74287a82fa66f229fe49bec1b34600
Ahmed Mahmood Adamu <33347856+ahrmerd@users.noreply.github.com>
2024-02-17 11:51:59 +0100

    Update updated_ngrok.sh

--- scripts/updated_ngrok.sh
+++ scripts/updated_ngrok.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 wget -q -nc https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip
-unzip -qq -n ngrok-v3-stable-linux-amd64.zip
+unzip -qq -o ngrok-v3-stable-linux-amd64.zip
 echo Done!
